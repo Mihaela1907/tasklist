0ac765a0557716edcd5f0ba6ed11096697c92690
Mihaela <mihaela.dijanesic@factory.hr>
2019-12-15 23:24:34 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>ToDo List</h1>
+    <TaskList/>
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TaskList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 100px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+        <div>
+        <span class="title"
+            contenteditable="true"
+            v-on:keydown.enter="updateTask($event, list)"
+            v-on:blur="updateTask($event, list)"
+            v-bind:class="{completed: list.isComplete}">{{list.title}}</span> 
+        <input type="checkbox" v-on:change="completeTask(list)" v-bind:checked="list.isComplete"/>
+        <span class="remove" v-on:click="removeTask(index)">x</span>        
+        </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        list: Object
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
